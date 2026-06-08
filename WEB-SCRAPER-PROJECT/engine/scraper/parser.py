9543ee6472ad9f676a2f659cfb4797ab7a8ef3d5
rzepeeek <147655843+rzepeeek@users.noreply.github.com>
2026-06-08 21:24:13 +0200

    Update parser.py
    dodanie title

--- WEB-SCRAPER-PROJECT/engine/scraper/parser.py
+++ WEB-SCRAPER-PROJECT/engine/scraper/parser.py
@@ -4,13 +4,15 @@ import re
 def parse_html(html):
     soup = BeautifulSoup(html, "html.parser")
 
-    emails = re.findall(r"[\w\.-]+@[\w\.-]+\.\w+", html)
-    phones = re.findall(r"\+?\d[\d\s\-]{8,}\d", html)
+    title = soup.title.text.strip() if soup.title else ""
+    emails = re.findall(r"[\\w\\.-]+@[\\w\\.-]+", html)
+    phones = re.findall(r"\\+?\\d[\\d\\s-]{8,}", html)
     links = [a.get("href") for a in soup.find_all("a") if a.get("href")]
 
     return {
-        "emails": emails,
-        "phones": phones,
+        "titles": title,
+        "emails": list(set(emails)),
+        "phones": list(set(phones)),
         "links": links,
         "addresses": []  
     }
