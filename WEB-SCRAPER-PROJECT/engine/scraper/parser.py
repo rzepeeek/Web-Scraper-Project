96c5f04ac32144c3cc465c1812eb1a31f4f68b80
rzepeeek <147655843+rzepeeek@users.noreply.github.com>
2026-06-08 21:39:29 +0200

    Update parser.py

--- WEB-SCRAPER-PROJECT/engine/scraper/parser.py
+++ WEB-SCRAPER-PROJECT/engine/scraper/parser.py
@@ -5,8 +5,8 @@ def parse_html(html):
     soup = BeautifulSoup(html, "html.parser")
 
     title = soup.title.text.strip() if soup.title else ""
-    emails = re.findall(r"[\\w\\.-]+@[\\w\\.-]+", html)
-    phones = re.findall(r"\\+?\\d[\\d\\s-]{8,}", html)
+    emails = re.findall(r"[\w\.-]+@[\w\.-]+\.\w+", html)
+    phones = re.findall(r"\+?\d[\d\s\-]{8,}\d", html)
     links = [a.get("href") for a in soup.find_all("a") if a.get("href")]
     headers = [h.text.strip() for h in soup.find_all("h1")]
     subheaders = [h.text.strip() for h in soup.find_all("h2") if h.text.strip()]
